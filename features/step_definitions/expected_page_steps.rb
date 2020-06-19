d0e40e586b15f37854841a4e91a352eab28d333a
Christopher Kelling <cj.kelling88@gmail.com>
2020-06-19 07:04:27 -0600

    Match _steps file wording to feature file update

--- features/step_definitions/expected_page_steps.rb
+++ features/step_definitions/expected_page_steps.rb
@@ -1,8 +1,8 @@
-Given(/^The user is on the fantasy name generator page$/) do
+Given(/^The user attempts to visit the Fantasy Name Generator page$/) do
   find('title', :text => 'Fantasy Name Generator', :visible => :hidden)
 end
 
-Then(/^I see the text "(.*?)" and I see the text "(.*?)"$/) do |text, text2|
+Then(/^They see the text "(.*?)" and they see the text "(.*?)"$/) do |text, text2|
   find('h2', :text => text)
   find('h3', :text => text2)
 end
