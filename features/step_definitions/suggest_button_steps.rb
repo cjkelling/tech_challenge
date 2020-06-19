232bfaa871a95b9a4ac38b9431e174b2844693d3
Christopher Kelling <cj.kelling88@gmail.com>
2020-06-19 07:05:28 -0600

    Add a specific Given to the test instead of using the one from the prior scenario

--- features/step_definitions/suggest_button_steps.rb
+++ features/step_definitions/suggest_button_steps.rb
@@ -15,9 +15,16 @@ end
 Then(/^A name is randomly generated in the field and they are able to submit the form$/) do
   find("[name=original_name]").value != ''
   page.click_button("Write me some fantasy names")
+  find('h2', :text => "Best Suggestions")
 end
 
 # The user should be able to find the suggested name generated on the suggested names page
+Given(/^A User is on the Fantasy Name Generator page and they press the suggest button$/) do
+  find('h2', :text => "Name Generator")
+  find('h3', :text => 'Find the Perfect Fantasy Name')
+  page.click_button("Suggest")
+end
+
 When(/^They submit the form$/) do
   random_name = find("[name=original_name]").value
   @first_name = random_name.split(' ')[0]
@@ -28,17 +35,13 @@ end
 Then(/^They can find either the first or last name in the list of generated names$/) do
   found = false
   page.all('div.name').each do |name|
-    if name.text.downcase.include?(@first_name.downcase)
-      found = true
-      break
-    elsif
-      name.text.downcase.include?(@last_name.downcase)
+    if name.text.downcase.include?(@first_name.downcase) || name.text.downcase.include?(@last_name.downcase)
       found = true
       break
     end
   end
 
   if found != true
-    fail(StandardError.new("Failed Test"))
+    fail(StandardError.new("Failed Test - Name Not Found"))
   end
 end
